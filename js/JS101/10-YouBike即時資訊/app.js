3d0da3acb4be9fbe1ad50dc10b94deb42292ec47
Hannah Ko <hipin88627@gmail.com>
2025-10-26 18:32:33 +0800

    ubikehomework

--- js/JS101/10-YouBike即時資訊/app.js
+++ js/JS101/10-YouBike即時資訊/app.js
@@ -1,41 +1,51 @@
 // 程式碼寫這裡
 // document.addEventListener("DOMContentLoaded", () => {
 
-const url = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
 
-const form = document.querySelector("#searchForm")
 
-const keyword = document.querySelector("#searchKeyword")
+const form = document.querySelector("#searchForm");
+
+const keyword = document.querySelector("#searchKeyword");
 
 // const btn = document.querySelector("#btn")
-    
+
 // 表單有預設的summit，要先停止預設:用preventDefault
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const query = keyword.value.trim()
-                                // trim:刪除空白字元
-    if(query !=""){
-    fetch(url) 
-     .then((resp) => {
-        return resp.json()
-     })
-     .then((stations) => {
-        stations.fliter((station) => {
-            return station.ar.includes(query)
-        })
-        
-    })
+  const query = keyword.value.trim();
+  // trim:刪除空白字元
+  const url =
+  "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json";
 
-    }
-})
+  if (query != "") {
+    fetch(url)
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((stations) => {
 
+        const list = document.querySelector(".siteList")
+        list.innerHTML = ""
 
-   
+        stations
+        .filter((station) => {
+          return station.ar.includes(query)})
+        .forEach((station) => {
+          const item = `<li class="list-group-item fs-5">
+          <i class="fas fa-bicycle"></i>
+          ${station.sna.replace("YouBike2.0_", "")}(${station.available_rent_bikes})<br>
+          <small class="text-muted">${station.ar}</small>
+          </li>`
 
+          list.insertAdjacentHTML("beforeend", item)
 
+        });
+      });
+  }
+});
 
-// fetch(url) 
+// fetch(url)
 //     .then((resp) => {
 //         return resp.json()
 //     })
@@ -44,6 +54,5 @@ form.addEventListener("submit", (e) => {
 //         .fliter((station) => {
 //             return station.sna
 //         })
-//         console.log(stations.length);
-        
+
 //     })
